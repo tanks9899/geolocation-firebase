e6f66da9d43280e8554103e583db6cc5749fbbd6
Tan Kai Sheng <sheng.phantom@gmail.com>
2024-03-07 18:07:43 +0800

    Update GoogleAPI to OpenCageAPI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
     <button @click="getCurrentLocation" style="margin-top: 10vh">Get My Current Location</button>
-    <h2>{{ address }}</h2>
+    <button>SignOut</button>
+    <h2>
+      {{ address }}
+    </h2>
     <GMapMap :center="center" :zoom="7" map-type-id="terrain" style="width: 500px; height: 300px">
       <GMapCluster>
         <GMapMarker
@@ -20,8 +23,8 @@
 <script setup>
 import axios from 'axios'
 import { ref } from 'vue'
-
-const address = ref('')
+// free do mounted on signin and redicrect to home page if logged in
+const address = ref({})
 const center = ref({ lat: 0, lng: 0 })
 const markers = ref([
   {
@@ -32,16 +35,24 @@ const markers = ref([
   }
 ])
 
-const getAddress = (lat, long) => {
+const getAddress = (lat, lng) => {
   axios
     .get(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${import.meta.env.VITE_GOOGLE_API_KEY}`
+      `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${import.meta.env.VITE_OPEN_CAGE_API_KEY}`
     )
     .then((res) => {
-      if (res.data.error_message) {
-        alert(res.data.error_message)
-      } else {
-        address.value = res.data.results[0].formatted_address
+      const result = res.data.results[0]
+      address.value = {
+        country: result.components.country,
+        country_area: result.annotations.UN_M49.regions.ASIA,
+        country_calling_code: result.annotations.callingcode,
+        country_capital: result.annotations.flag,
+        country_code: result.components.country_code.toUpperCase(),
+        country_code_iso3: result.components['ISO_3166-1_alpha-3'],
+        country_population: result.annotations.UN_M49.regions.MY,
+        country_tld: result.components.country_code.toUpperCase(),
+        currency_name: result.annotations.currency.name,
+        currency_symbol: result.annotations.currency.symbol
       }
     })
     .catch((err) => {
